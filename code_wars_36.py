ad55f2d12be8449d73f01a05d0ebf0028e66f0c6
Gruka469 <lifetroll6@gmail.com>
2023-12-07 16:36:35 +0200

    Update code_wars_36.py

--- code_wars_36.py
+++ code_wars_36.py
@@ -2,7 +2,6 @@ def gimme_the_letters(sp):
     start, end = sp.split('-')
     result = ''
 
-    # Convert the start and end characters to their ASCII values
     start_ascii, end_ascii = ord(start), ord(end)
 
     # Build the string using a loop
