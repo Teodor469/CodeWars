d0595f9dffe81ee1dd9c164f011707eeaeb41113
Gruka469 <lifetroll6@gmail.com>
2023-12-14 23:27:57 +0200

    Update code_wars_40.py

--- code_wars_40.py
+++ code_wars_40.py
@@ -1,7 +1,13 @@
 def reverse_middle(lst):
     middle = len(lst) // 2
-    middle_part = lst[middle-1:middle+2][::-1]
+    if len(lst) > 4:
+        middle_part = lst[middle-1:middle+2][::-1]
+    elif len(lst) == 4:
+        middle_part = lst[middle-1:middle+1][::-1]
     left_part = lst[:middle-1]
     right_part = lst[middle+2:]
 
     return left_part + middle_part + right_part
+
+lst = input()
+print(reverse_middle(lst))
