dd7d1e478431d11c69aae83398787eb32ab42467
Gruka469 <lifetroll6@gmail.com>
2023-12-15 15:04:17 +0200

    Update code_wars_40.py

--- code_wars_40.py
+++ code_wars_40.py
@@ -1,13 +1,10 @@
-def reverse_middle(lst):
-    middle = len(lst) // 2
-    if len(lst) > 4:
-        middle_part = lst[middle-1:middle+2][::-1]
-    elif len(lst) == 4:
-        middle_part = lst[middle-1:middle+1][::-1]
-    left_part = lst[:middle-1]
-    right_part = lst[middle+2:]
-
-    return left_part + middle_part + right_part
-
-lst = input()
-print(reverse_middle(lst))
+def array_manip(array):
+    new_array = [-1] * len(array)
+    for i in range(len(array)):
+        least_greater = float('inf')
+        for j in range(i + 1, len(array)):
+            if array[j] > array[i] and array[j] < least_greater:
+                least_greater = array[j]
+        if least_greater != float('inf'):
+            new_array[i] = least_greater
+    return new_array
