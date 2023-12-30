3aa39febeb1e7db8655f0ef492106659b6352272
Gruka469 <lifetroll6@gmail.com>
2023-12-30 15:45:42 +0200

    Update code_wars_56.py

--- code_wars_56.py
+++ code_wars_56.py
@@ -1,10 +1,5 @@
 def invert(lst):
-    for i in lst:
-        if int(i) > 0:
-            i *= -1
-        else:
-            i *= 1
-    return lst
+    return [-x for x in lst]
 
 a = input()
 print(invert(a))
